aa41c0f98df95a0bdd5329d38b5915d40cddab12
astratechlearn <adiyasaa56@gmail.com>
2023-09-29 09:51:37 +0700

    Add project files.

---
+++ Tugas_2_Kelompok_01/Controllers/KuraKuraController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tugas_2_Kelompok_01.Controllers
+{
+    public class KuraKuraController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
