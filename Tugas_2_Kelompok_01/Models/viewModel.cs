e366795e8c20248bfe453a6f60475d846d8b55ae
astratechlearn <adiyasaa56@gmail.com>
2023-10-02 10:17:08 +0700

    Modal Update

---
+++ Tugas_2_Kelompok_01/Models/viewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tugas_2_Kelompok_01.Models
+{
+    public class MyViewModel
+    {
+        public List<KuraKura> ListKuraKura { get; set; }
+        public List<JenisKuraKura> ListJenisKuraKura { get; set; }
+    }
+
+}
