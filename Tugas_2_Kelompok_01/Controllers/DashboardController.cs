728499115d661c03ff714122de92717936f9c6b9
astratechlearn <adiyasaa56@gmail.com>
2023-10-02 08:08:02 +0700

    layout

---
+++ Tugas_2_Kelompok_01/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tugas_2_Kelompok_01.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
